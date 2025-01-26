57b6f3de48687030ad5a6c0d574745affdb2d9cc
shar <faethychan@gmail.com>
2025-01-26 14:17:01 +0800

    Add javascript to change card image

---
+++ js/search-query.js
@@ -0,0 +1,4 @@
+for (let image of document.querySelectorAll(".card img")) {
+  image.src =
+    "https://images.unsplash.com/photo-1561948955-570b270e7c36?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhdHxlbnwwfHwwfHx8MA%3D%3D";
+}
