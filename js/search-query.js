c836c8af4edd7435dc77ed8c51454e114ad4be73
Charlotte <faethychan@gmail.com>
2025-01-29 19:35:58 +0800

    Moved code

--- js/search-query.js
+++ js/search-query.js
@@ -2,6 +2,24 @@
 const apiKey = "67960fb80acc0626570d3648";
 const listingUrl = "https://mokesellfed-153b.restdb.io/rest/listing";
 
+//Get search query from searchbar
+const urlParams = new URLSearchParams(window.location.search);
+searchQuery = urlParams.get("query");
+searchCategory = urlParams.get("category");
+
+const searchbarInput = document.querySelector("#searchbar-input");
+const searchbarCategory = document.querySelector("#searchbar-category");
+searchbarInput.value = searchQuery;
+searchbarCategory.value = searchCategory;
+
+searchCategory = searchCategory[0].toUpperCase() + searchCategory.slice(1); //Capitalize first letter to be shown in span
+
+const searchQuerySpan = document.querySelector("#search-query");
+searchQuerySpan.innerText = searchQuery;
+const searchCategorySpan = document.querySelector("#search-category");
+searchCategorySpan.innerText = searchCategory;
+
+// Fetch Listings
 fetch(listingUrl, {
   headers: {
     "x-apikey": apiKey,
@@ -22,20 +40,3 @@ fetch(listingUrl, {
   .catch((e) => {
     console.log(e);
   });
-
-//Get search query from searchbar
-const urlParams = new URLSearchParams(window.location.search);
-const searchQuery = urlParams.get("query");
-let searchCategory = urlParams.get("category");
-
-const searchbarInput = document.querySelector("#searchbar-input");
-const searchbarCategory = document.querySelector("#searchbar-category");
-searchbarInput.value = searchQuery;
-searchbarCategory.value = searchCategory;
-
-searchCategory = searchCategory[0].toUpperCase() + searchCategory.slice(1);
-
-const searchQuerySpan = document.querySelector("#search-query");
-searchQuerySpan.innerText = searchQuery;
-const searchCategorySpan = document.querySelector("#search-category");
-searchCategorySpan.innerText = searchCategory;
