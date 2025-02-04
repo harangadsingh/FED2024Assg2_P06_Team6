3e7efc7d2b5a86f6cd5cf039d5af06bf87f72f21
Charlotte <faethychan@gmail.com>
2025-02-04 21:35:55 +0800

    Change createAppendElement to accept multiple classes

--- js/search-query.js
+++ js/search-query.js
@@ -200,6 +200,6 @@ function createListingElements(listing) {
 function createAppendElement(elementType, text, parent = "", classes = "") {
     const element = document.createElement(elementType);
     element.innerText = text;
-    classes != "" && element.classList.add(classes); //If there is a class, add it
+    classes != "" && element.classList.add(...classes); //If there is a class, add it
     parent != "" && parent.append(element); //If there is a parent, append the element to it
 }
