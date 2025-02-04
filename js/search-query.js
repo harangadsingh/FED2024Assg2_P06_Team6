c8c6730be004857c55ceb48e5664e0ae8f3e1e53
Charlotte <faethychan@gmail.com>
2025-02-04 20:53:58 +0800

    Add class argument to createAppendElement and make parent & class nullable

--- js/search-query.js
+++ js/search-query.js
@@ -172,8 +172,9 @@ function createListingElements(listing) {
     return container;
 }
 
-function createAppendElement(elementType, text, parent) {
+function createAppendElement(elementType, text, parent = "", classes = "") {
     const element = document.createElement(elementType);
     element.innerText = text;
-    parent.append(element);
+    classes != "" && element.classList.add(classes); //If there is a class, add it
+    parent != "" && parent.append(element); //If there is a parent, append the element to it
 }
