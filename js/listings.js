d5238499409f53412df198bde57e1a0c7e640157
Charlotte <faethychan@gmail.com>
2025-02-08 02:30:35 +0800

    Extract create element function into its own script

--- js/listings.js
+++ js/listings.js
@@ -229,14 +229,6 @@ function createListingElements(listingSellerPair, likesCount) {
     return container;
 }
 
-function createAppendElement(elementType, text, parent = "", classes = "") {
-    const element = document.createElement(elementType);
-    element.innerText = text;
-    classes != "" && element.classList.add(...classes); //If there is a class, add it
-    parent != "" && parent.append(element); //If there is a parent, append the element to it
-    return element;
-}
-
 function createImageCarousel(images) {
     const carouselContainer = createAppendElement("div", "", "", ["carousel", "slide"]);
     carouselContainer.id = `carousel${listingNumber}`;
