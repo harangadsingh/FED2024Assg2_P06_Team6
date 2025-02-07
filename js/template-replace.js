638e7ec328fbb9eeac846503dbab3593ebc1b610
Charlotte <faethychan@gmail.com>
2025-02-07 15:19:17 +0800

    Check if localStorage has userAccount to show notifs & likes

--- js/template-replace.js
+++ js/template-replace.js
@@ -7,6 +7,11 @@ fetch("html-templates/main-navbar.html")
         let newelem = document.createElement("div");
         newelem.innerHTML = text;
         oldelem.parentNode.replaceChild(newelem, oldelem);
+
+        localStorage.getItem("userAccount") != null &&
+            document.querySelectorAll(".require-account").forEach((element) => {
+                element.classList.remove("d-none");
+            });
     });
 
 // <div id="replace-with-checkout"></div>;
