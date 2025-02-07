46aeb226ae8a78c89e7cce79c7ac1e09f1aea38c
Charlotte <faethychan@gmail.com>
2025-02-08 02:59:56 +0800

    Add comments

--- js/searchbarInput.js
+++ js/searchbarInput.js
@@ -3,12 +3,16 @@ const searchQuery = urlParams.get("query") ? urlParams.get("query") : "";
 const searchCategory = urlParams.get("category") ? urlParams.get("category") : "Category";
 
 if (searchQuery == "") {
+    //Hide the "Results for..." if there was nothing searched
     document.querySelector(".search-query-header").classList.add("invisible");
 } else if (searchQuery != "") {
+    //If there was something searched, put it in the Results for...header
     document.querySelector("#search-query").innerText = searchQuery;
 } else if (searchCategory == "Category") {
+    //If no category was selected hide the Results... IN...
     document.querySelector(".search-category-span").classList.add("invisible");
 } else {
+    //If a category was selected, put it in the Results... IN... span
     document.querySelector("#search-category").innerText = searchCategory;
 }
 
@@ -23,11 +27,13 @@ function waitForElement(selector, callback) {
 }
 
 waitForElement("#searchbar-input", (element) => {
+    //Wait for the searchbar to load, then set its value to what was searched
     console.log("Element found!", element);
     element.value = searchQuery;
 });
 
 waitForElement("#searchbar-category", (element) => {
+    //Wait for the searchbar to load, then set its value to what was set as the category to search for
     console.log("Element found!", element);
     element.value = searchCategory;
 });
